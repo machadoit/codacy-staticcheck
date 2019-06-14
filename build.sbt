567013c2052ae1a23fa72c915544f12aa582b0d1
Goncalo Prendi <goncalo@codacy.com>
2019-06-14 14:55:37 +0100

    feature: Bump codacy-engine-seed FT-7452 :breaking:

--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ name := "codacy-staticcheck"
 
 version := "1.0-SNAPSHOT"
 
-val languageVersion = "2.11.12"
+val languageVersion = "2.12.7"
 
 scalaVersion := languageVersion
 
@@ -19,8 +19,8 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.codacy" %% "codacy-engine-scala-seed" % "2.7.7" withSources(),
-  "org.scala-lang.modules" %% "scala-xml" % "1.0.4" withSources()
+  "com.codacy" %% "codacy-engine-scala-seed" % "3.0.9" withSources (),
+  "org.scala-lang.modules" %% "scala-xml" % "1.0.6" withSources ()
 )
 
 enablePlugins(JavaAppPackaging)
@@ -31,14 +31,18 @@ version in Docker := "1.0"
 
 organization := "com.codacy"
 
-lazy val toolVersion = TaskKey[String]("Retrieve the version of the underlying tool from patterns.json")
+lazy val toolVersion = TaskKey[String](
+  "Retrieve the version of the underlying tool from patterns.json")
 
 toolVersion := {
   val jsonFile = (resourceDirectory in Compile).value / "docs" / "patterns.json"
-  val toolMap = JSON.parseFull(Source.fromFile(jsonFile).getLines().mkString)
+  val toolMap = JSON
+    .parseFull(Source.fromFile(jsonFile).getLines().mkString)
     .getOrElse(throw new Exception("patterns.json is not a valid json"))
     .asInstanceOf[Map[String, String]]
-  toolMap.getOrElse[String]("version", throw new Exception("Failed to retrieve 'version' from patterns.json"))
+  toolMap.getOrElse[String](
+    "version",
+    throw new Exception("Failed to retrieve 'version' from patterns.json"))
 }
 
 def installAll(toolVersion: String) =
@@ -54,14 +58,15 @@ def installAll(toolVersion: String) =
      |rm -rf /var/cache/apk/* &&
      |rm -rf /tmp/*""".stripMargin.replaceAll(System.lineSeparator(), " ")
 
-mappings in Universal <++= (resourceDirectory in Compile) map { (resourceDir: File) =>
-  val src = resourceDir / "docs"
-  val dest = "/docs"
+mappings in Universal <++= (resourceDirectory in Compile) map {
+  (resourceDir: File) =>
+    val src = resourceDir / "docs"
+    val dest = "/docs"
 
-  for {
-    path <- (src ***).get
-    if !path.isDirectory
-  } yield path -> path.toString.replaceFirst(src.toString, dest)
+    for {
+      path <- (src ***).get
+      if !path.isDirectory
+    } yield path -> path.toString.replaceFirst(src.toString, dest)
 }
 
 val dockerUser = "docker"
@@ -77,13 +82,15 @@ mainClass in Compile := Some("codacy.Engine")
 
 dockerCommands := {
   dockerCommands.dependsOn(toolVersion).value.flatMap {
-    case cmd@(Cmd("ADD", _)) => List(
-      Cmd("RUN", "adduser -u 2004 -D docker"),
-      cmd,
-      Cmd("RUN", installAll(toolVersion.value)),
-      Cmd("RUN", "mv /opt/docker/docs /docs"),
-      ExecCmd("RUN", Seq("chown", "-R", s"$dockerUser:$dockerGroup", "/docs"): _*)
-    )
+    case cmd @ (Cmd("ADD", _)) =>
+      List(
+        Cmd("RUN", "adduser -u 2004 -D docker"),
+        cmd,
+        Cmd("RUN", installAll(toolVersion.value)),
+        Cmd("RUN", "mv /opt/docker/docs /docs"),
+        ExecCmd("RUN",
+                Seq("chown", "-R", s"$dockerUser:$dockerGroup", "/docs"): _*)
+      )
     case other => List(other)
   }
 }
--- src/main/scala/codacy/staticcheck/Staticcheck.scala
+++ src/main/scala/codacy/staticcheck/Staticcheck.scala
@@ -1,84 +1,80 @@
 package codacy.staticcheck
 
-import codacy.docker.api
-import codacy.docker.api.Result.Issue
-import codacy.docker.api._
-import codacy.docker.api.utils.ToolHelper
-import codacy.dockerApi.utils.CommandRunner
+import com.codacy.plugins.api.{Options, Source}
+import com.codacy.plugins.api.results.Result.Issue
+import com.codacy.plugins.api.results.{Parameter, Pattern, Result, Tool}
+import com.codacy.tools.scala.seed.utils.CommandRunner
+import com.codacy.tools.scala.seed.utils.ToolHelper._
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
 import scala.util.{Failure, Properties, Success, Try}
 
 object Staticcheck extends Tool {
 
-  override def apply(source: api.Source.Directory,
+  override def apply(source: Source.Directory,
                      configuration: Option[List[Pattern.Definition]],
-                     filesOpt: Option[Set[api.Source.File]])(
-                      implicit specification: Tool.Specification): Try[List[Result]] = {
+                     files: Option[Set[Source.File]],
+                     options: Map[Options.Key, Options.Value])(implicit specification: Tool.Specification): Try[List[Result]] = {
 
-    val patternsToLintOpt: Option[List[Pattern.Definition]] =
-      ToolHelper.patternsToLint(configuration)
+    val patternsToLintOpt: Option[List[Pattern.Definition]] = configuration.withDefaultParameters
 
-    patternsToLintOpt.fold[Try[List[Result]]](Success(List.empty[Result])) { patternsToLint =>
+    patternsToLintOpt.fold[Try[List[Result]]](Success(List.empty[Result])) {
+      patternsToLint =>
+        val patternsToLintSet: Set[Pattern.Id] =
+          patternsToLint.map(_.patternId)(
+            collection
+              .breakOut[List[Pattern.Definition], Pattern.Id, Set[Pattern.Id]])
 
-      val patternsToLintSet: Set[Pattern.Id] = patternsToLint.map(_.patternId)(collection.breakOut[List[Pattern.Definition], Pattern.Id, Set[Pattern.Id]])
+        val patternsToIgnore: Set[Pattern.Id] = specification.patterns.map(
+          _.patternId) diff patternsToLintSet
 
-      val patternsToIgnore: Set[Pattern.Id] = specification.patterns.map(_.patternId) diff patternsToLintSet
+        val ignorePatternsCmd: Seq[String] = if (patternsToIgnore.nonEmpty) {
+          Seq("-ignore", s"*:${patternsToIgnore.map(_.value).mkString(",")}")
+        } else {
+          Seq.empty[String]
+        }
 
-      val ignorePatternsCmd: Seq[String] = if (patternsToIgnore.nonEmpty) {
-        Seq("-ignore", s"*:${patternsToIgnore.map(_.value).mkString(",")}")
-      } else {
-        Seq.empty[String]
-      }
+        val command = List("/opt/docker/go/bin/staticcheck", "-f", "json") ++ ignorePatternsCmd
 
-      val command = List("/opt/docker/go/bin/staticcheck", "-f", "json") ++ ignorePatternsCmd
-
-      CommandRunner.exec(command, Some(new java.io.File(source.path))) match {
-        case Right(resultFromTool) if resultFromTool.stderr.isEmpty =>
-          parseToolResult(resultFromTool.stdout).map(_.filter(result => resultFilter(result, patternsToLintSet, filesOpt))) match {
-            case s@Success(_) => s
-            case Failure(e) =>
-              val msg =
-                s"""
-                   |${e.getStackTrace.mkString(System.lineSeparator)}
-                   |Staticcheck exited with code ${resultFromTool.exitCode}
-                   |message: ${e.getMessage}
-                   |stdout: ${
-                  resultFromTool.stdout.mkString(
-                    Properties.lineSeparator)
-                }
-                   |stderr: ${
-                  resultFromTool.stderr.mkString(
-                    Properties.lineSeparator)
-                }
+        CommandRunner.exec(command, Some(new java.io.File(source.path))) match {
+          case Right(resultFromTool) if resultFromTool.stderr.isEmpty =>
+            parseToolResult(resultFromTool.stdout).map(_.filter(result =>
+              resultFilter(result, patternsToLintSet, files))) match {
+              case s @ Success(_) => s
+              case Failure(e) =>
+                val msg =
+                  s"""
+                     |${e.getStackTrace.mkString(System.lineSeparator)}
+                     |Staticcheck exited with code ${resultFromTool.exitCode}
+                     |message: ${e.getMessage}
+                     |stdout: ${resultFromTool.stdout.mkString(
+                    Properties.lineSeparator)}
+                     |stderr: ${resultFromTool.stderr.mkString(
+                    Properties.lineSeparator)}
                 """.stripMargin
-              Failure(new Exception(msg))
-          }
+                Failure(new Exception(msg))
+            }
 
-        case Right(resultFromTool) =>
-          val msg =
-            s"""
-               |Staticcheck exited with code ${resultFromTool.exitCode} with output on the stderr
-               |stdout: ${
-              resultFromTool.stdout.mkString(
-                Properties.lineSeparator)
-            }
-               |stderr: ${
-              resultFromTool.stderr.mkString(
-                Properties.lineSeparator)
-            }
+          case Right(resultFromTool) =>
+            val msg =
+              s"""
+                 |Staticcheck exited with code ${resultFromTool.exitCode} with output on the stderr
+                 |stdout: ${resultFromTool.stdout.mkString(
+                Properties.lineSeparator)}
+                 |stderr: ${resultFromTool.stderr.mkString(
+                Properties.lineSeparator)}
                 """.stripMargin
 
-          Failure(new Exception(msg))
+            Failure(new Exception(msg))
 
-        case Left(e) =>
-          Failure(e)
-      }
-
+          case Left(e) =>
+            Failure(e)
+        }
     }
   }
 
-  private def parseToolResult(resultsFromTool: List[String]): Try[List[Result]] = {
+  private def parseToolResult(
+      resultsFromTool: List[String]): Try[List[Result]] = {
     Try {
       val results: List[Try[Result]] = resultsFromTool.map { resultRaw =>
         Json.parse(resultRaw).validate[ToolNativeResult] match {
@@ -93,13 +89,16 @@ object Staticcheck extends Tool {
 
       val maybeFailure: Option[Try[Result]] = results.find(_.isFailure)
 
-      maybeFailure.fold[Try[List[Result]]](Success(results.flatMap(_.toOption))) { failure =>
-        failure.map(List(_))
+      maybeFailure.fold[Try[List[Result]]](Success(results.flatMap(_.toOption))) {
+        failure =>
+          failure.map(List(_))
       }
     }.flatten
   }
 
-  private def resultFilter(result: Result, patternsToLintSet: Set[Pattern.Id], filesOpt: Option[Set[Source.File]]): Boolean = {
+  private def resultFilter(result: Result,
+                           patternsToLintSet: Set[Pattern.Id],
+                           filesOpt: Option[Set[Source.File]]): Boolean = {
     def patternFilter(patternId: Pattern.Id): Boolean = {
       patternsToLintSet.contains(patternId)
     }
@@ -117,7 +116,9 @@ object Staticcheck extends Tool {
   }
 
   private def toResult(nativeResult: ToolNativeResult): Result = {
-    Result.Issue(api.Source.File(nativeResult.location.file), Result.Message(nativeResult.message),
-      Pattern.Id(nativeResult.code), api.Source.Line(nativeResult.location.line))
+    Result.Issue(Source.File(nativeResult.location.file),
+                 Result.Message(nativeResult.message),
+                 Pattern.Id(nativeResult.code),
+                 Source.Line(nativeResult.location.line))
   }
 }
